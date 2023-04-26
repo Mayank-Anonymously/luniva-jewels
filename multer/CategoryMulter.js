c28e0525d521254c73897f21ff6c898067f899ca
MayankTravel <mayank.kumar@midastravel.org>
2023-04-26 11:26:56 +0530

    Multer Changes

--- multer/CategoryMulter.js
+++ multer/CategoryMulter.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, "images/");
+    callback(null, "images");
   },
   filename: function (req, file, callback) {
     callback(null, file.originalname);
--- multer/ProductImages.js
+++ multer/ProductImages.js
@@ -3,7 +3,7 @@ var { existsSync, mkdirSync } = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, "/images/");
+    callback(null, "images");
   },
   filename: function (req, file, callback) {
     callback(null, file.originalname);
